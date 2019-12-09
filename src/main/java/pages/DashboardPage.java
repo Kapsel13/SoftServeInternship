8a99d341001b5130c522ae99cbf35e33832dd3e6
Marcin Kowalski <mkowal@macmini920.softservecom.com>
2019-12-09 14:52:17 +0100

    fixed

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -219,7 +219,7 @@ public class DashboardPage extends BasePage{
         WebElement activeDashboardToClick = scrollElementIntoView(By.xpath(String.format(activeDashboardInList,numberOfActiveDashboard)));
         activeDashboardToClick.click();
         try {
-            Thread.sleep(10000);
+            Thread.sleep(30000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
