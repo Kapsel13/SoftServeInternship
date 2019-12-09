8ec188eac2e51f3baee816a50f718056a459b8df
Marcin Kowalski <mkowal@macmini920.softservecom.com>
2019-12-09 15:10:40 +0100

    fixed

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -218,15 +218,15 @@ public class DashboardPage extends BasePage{
         int numberOfActiveDashboard = rnd.nextInt(activeDashboardsNumber-1)+1;;
         WebElement activeDashboardToClick = scrollElementIntoView(By.xpath(String.format(activeDashboardInList,numberOfActiveDashboard)));
         activeDashboardToClick.click();
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         try {
             FileUtils.copyFile(srcFile,new File("target/screenshots/screenshot.png"));
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            Thread.sleep(30000);
-        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
