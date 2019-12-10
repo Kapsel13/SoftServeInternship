acc1b24339da7f208151adfb147b178d6f3bdee1
Marcin Kowalski <mkowal@macmini920.softservecom.com>
2019-12-10 09:16:21 +0100

    new time for Thread Sleep

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -219,7 +219,7 @@ public class DashboardPage extends BasePage{
         WebElement activeDashboardToClick = scrollElementIntoView(By.xpath(String.format(activeDashboardInList,numberOfActiveDashboard)));
         activeDashboardToClick.click();
         try {
-            Thread.sleep(90000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
