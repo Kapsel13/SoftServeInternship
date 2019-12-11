84cd93e1c1b359a14873e79f606d2a59f2007269
Marcin Kowalski <mkowal@macmini920.softservecom.com>
2019-12-11 10:43:30 +0100

    fixed

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -263,7 +263,12 @@ public class DashboardPage extends BasePage{
     }
 
     public void deleteDashboard(String dashboardName){
-        wait.until(ExpectedConditions.visibilityOfElementLocated(dashboardDropdownButton));
+        try{
+            wait.until(ExpectedConditions.visibilityOfElementLocated(dashboardDropdownButton));
+        }catch(TimeoutException e){
+            driver.navigate().refresh();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(dashboardDropdownButton));
+        }
         driver.findElement(dashboardDropdownButton).click();
         try {
             Thread.sleep(2000);
